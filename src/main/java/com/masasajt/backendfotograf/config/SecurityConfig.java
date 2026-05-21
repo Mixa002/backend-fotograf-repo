e0bd7641abee26a21f41ccbdc9cb904d87ed0a8d
Mihajlo Djordjevic <mimi.djordjevic23@gmail.com>
2026-05-21 15:02:00 +0200

    Sitne promene jer nije radio api za albume v2

--- src/main/java/com/masasajt/backendfotograf/config/SecurityConfig.java
+++ src/main/java/com/masasajt/backendfotograf/config/SecurityConfig.java
@@ -66,7 +66,10 @@ public class SecurityConfig {
         if(allowedOrigin == null || allowedOrigin.isEmpty()){
             allowedOrigin = "http://localhost:5173";
         }
-        configuration.setAllowedOrigins(List.of(allowedOrigin,"http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("https://*.vercel.app",
+                "https://*.vercel.live",
+                "http://localhost:5173",
+                "http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Cache-Control"));
         configuration.setAllowCredentials(true);
